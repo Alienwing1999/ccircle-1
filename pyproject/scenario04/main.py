631d32da39f6dc0213690e2713b8e2108df1bba4
Alienwing1999 <Alienwing1999@gmail.com>
2017-07-11 14:25:28 -0700

    Merge branch 'master' of https://github.com/mathcircle/ccircle
    # Conflicts:
    #	pyproject/scenario01/solution.py

--- pyproject/scenario04/main.py
+++ pyproject/scenario04/main.py
@@ -2,7 +2,40 @@ import ccircle
 import connection
 
 con = connection.create()
-con.send('set_name', {'name': 'put_your_name_or_nickname_here'})
+con.send('set_name', {'name': 'Alien'})
+
+
+
+
+
+args = {
+    'vx': 0,
+    'vy':0,
+
+}
+while True:
+    if ccircle.isKeyDown('down'):
+        args['vy'] = 50
+        args['vx'] = 0
+        con.send('set_velocity', args)
+
+    if ccircle.isKeyDown('up'):
+        args['vy'] = -50
+        args['vx'] = 0
+        con.send('set_velocity', args)
+
+    if ccircle.isKeyDown('right'):
+        args['vy'] = 0
+        args['vx'] = 50
+        con.send('set_velocity', args)
+
+    if ccircle.isKeyDown('left'):
+        args['vy'] = 0
+        args['vx'] = -50
+        con.send('set_velocity', args)
+    if ccircle.isKeyDown('space'):
+        con.send('damage_boss', args)
+
 
 # Write code to make money and kill the evil cat!
 # See readme.txt !
